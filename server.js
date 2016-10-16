d91cff2704b79958069023be8b285ef34bd9b6f7
Darshan <dave1089@live.com>
2016-10-16 23:29:51 +0000

    solution

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
+var port = 8080
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// app.listen(port, function () {
+//   console.log('Example app listening on port ' + port);
+// });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  var os = req.headers["user-agent"].split('(');
+  var lang = req.headers["accept-language"].substr(0,req.headers["accept-language"].indexOf(","))
+  res.end(JSON.stringify({"OS": os[1].split(')')[0] , "language" :  lang, "ip" : req.headers["x-forwarded-for"]}));
+  // res.end(JSON.stringify(req.headers));
+  
+}).listen(port);
